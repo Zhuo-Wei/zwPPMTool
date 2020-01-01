975cd0d5c8842261d8daa51c444ee4b9e63dfd9b
alva <zwei22@wustl.edu>
2020-01-01 11:08:46 -0600

    Backlog and ProjectTask

---
+++ PPMToolFullStack/src/main/java/zhuowei/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package zhuowei.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import zhuowei.ppmtool.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {// long:id
+}
